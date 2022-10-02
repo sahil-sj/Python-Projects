6acb79d0d5e9e068204bbdf3d9b52ff9aa59211e
Sahil <sahilmendiratta222@gmail.com>
2022-10-02 12:20:07 +0530

    programs added

---
+++ add.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Name = Sahil Mendiratta"<<endl<<"enrollment no = 10829802021"<<endl;
+	int a, b ,c;
+	cout << "Enter two numbers to add"<<endl;
+	cin >> a >> b;
+	c = a + b;
+	cout << "Sum = " << c << endl; 
+}
---
+++ areacircum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Name = Sahil Mendiratta"<<endl<<"enrollment no = 10829802021"<<endl;
+	int r;
+	float a,c;
+	cout<<"Enter the radius of a circle\n";
+	cin>>r;
+	a=3.14*r*r;
+	c=2*3.14*r;
+	cout<<"The Area of circle is : "<<a;
+	cout<<"\nThe Circumference of circle is : "<<c;
+	return 0;
+}
